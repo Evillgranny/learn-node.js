1ac98090e1b5bffbfcfead61d1ac3ca0d0a81b2c
Evillgranny <tractor2554393@yandex.by>
2020-10-30 17:11:33 +0300

    v-2.0

--- index.js
+++ index.js
@@ -2,19 +2,45 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+app.set('view engine', 'ejs')
 
 const urlencodedParser = bodyParser.urlencoded({extended: false})
 
-app.get("/", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/index.html")
+app.get('/form', (request,response) => {
+    response.render('form', {
+        invalidUserName: false,
+        invalidUserSurname: false,
+    })
 })
 
-app.post("/", urlencodedParser, function (request, response) {
-    if(!request.body.userName || !request.body.userSurname) {
-        return response.sendStatus(400)
+app.post('/form', urlencodedParser, (request, response) => {
+    let invalid = false
+    for (let i in request.body) {
+        request.body[i].length === 0 ? invalid = true : false
     }
-    console.log(request.body)
-    response.send(`${request.body.userName} - ${request.body.userSurname}`)
+
+    if (!invalid) {
+        response.render('success-form', {
+            userName: request.body.userName,
+            userSurname: request.body.userSurname
+        })
+    } else {
+        let errorUserName = false,
+            errorUserSurname = false,
+            userName = '',
+            userSurname = ''
+
+        !request.body.userName ? errorUserName = true : userName = request.body.userName
+        !request.body.userSurname ? errorUserSurname = true : userSurname = request.body.userSurname
+
+        response.render('form', {
+            invalidUserName: errorUserName,
+            invalidUserSurname: errorUserSurname,
+        })
+    }
 })
 
-app.listen(3000)
+app.listen(443, () => {
+    console.log('Server is running...')
+})
+
