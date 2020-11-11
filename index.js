06f71f61ad7ba0cb3e9fe9e55a5aef7b20be5cf7
Evillgranny <tractor2554393@yandex.by>
2020-11-11 23:31:49 +0300

    add btns

--- index.js
+++ index.js
@@ -45,6 +45,79 @@ webserver.post('/stat', jsonParser, async (req, res) => {
     })
 })
 
+webserver.get('/statFile', (req, res) => {
+    logLineSync(logFN,`[${port}] `+"stat file called xml");
+
+    // т.к. к этому сервису идёт AJAX-запрос со страниц с другим происхождением (origin), надо явно это разрешить
+    // res.setHeader("Access-Control-Allow-Origin","*");
+
+    console.log("request headers",req.headers);
+    const clientAccept=req.headers.accept;
+    if ( clientAccept==="application/json" ) {
+        res.setHeader("Content-Type", "application/json");
+        res.sendFile(__dirname + '/stat.json')
+    } else if ( clientAccept==="application/xml" ) {
+        res.setHeader("Content-Type", "application/xml");
+        let str = ''
+        let variants = []
+
+        fs.readFile(__dirname + '/variants.json', "utf8", (err, data) => {
+            const json = JSON.parse(data)
+            for (let key in json) {
+                variants.push(json[key])
+            }
+        })
+
+        fs.readFile(__dirname + '/stat.json', "utf8", (err, data) => {
+            const json = JSON.parse(data)
+            str  = '<stat>' + '\n'
+            for (let key in json) {
+                str += '<item>' + '\n' +
+                    '<title>' + variants[key] + '</title>' + '\n' +
+                    '<vote>' + json[key]  + '</vote>' + '\n' +
+                    '</item>' + '\n'
+            }
+            str += '</stat>'
+
+            res.send(str)
+        })
+    } else if ( clientAccept === "application/html" ) {
+        res.setHeader("Content-Type", "text/html");
+        let str = ''
+        let variants = []
+
+        fs.readFile(__dirname + '/variants.json', "utf8", (err, data) => {
+            const json = JSON.parse(data)
+            for (let key in json) {
+                variants.push(json[key])
+            }
+        })
+
+        fs.readFile(__dirname + '/stat.json', "utf8", (err, data) => {
+            const json = JSON.parse(data)
+            str  = '<!DOCTYPE html>\n' +
+                '<html lang="en">\n' +
+                '<head>\n' +
+                '    <meta charset="UTF-8">\n' +
+                '    <title>Title</title>\n' +
+                '</head>' +
+                '<body>' +
+                '<ul>'
+
+            for (let key in json) {
+                str +=
+                    '<li>' +
+                    '<span>' + variants[key] + '</span>' + ' ' +
+                    '<span>' + json[key]  + '</span>' +
+                    '</li>'
+            }
+            str += '</ul></body>'
+
+            res.send(str)
+        })
+    }
+});
+
 webserver.listen(port,()=>{
     logLineSync(logFN,"web server running on port "+port);
 });
--- public/script.js
+++ public/script.js
@@ -33,12 +33,49 @@ async function showInfo (variantsContainer, statContainer) {
     return variants
 }
 
+async function getJSON() {
+    const fetchOptions={
+        headers: {
+            'Accept': 'application/json',
+        },
+    };
+    const response = await fetch('/statFile',fetchOptions);
+    const data = await response.json();
+    return data
+}
+
+async function getXML() {
+    const fetchOptions={
+        headers: {
+            'Accept': 'application/xml',
+        },
+    };
+    const response = await fetch('/statFile',fetchOptions);
+    const xmlStr = await response.text();
+    return xmlStr
+}
+
+async function getHTML() {
+    const fetchOptions={
+        headers: {
+            'Accept': 'application/html',
+        },
+    };
+    const response = await fetch('/statFile',fetchOptions);
+    const htmlStr = await response.text();
+    return htmlStr
+}
+
 window.onload = () => {
     const variantsContainer = document.querySelector('#variants')
     const statContainer = document.querySelector('#stat')
     const form = document.querySelector('#form')
     const goodVoice = document.querySelector('#goodVoice')
     const btn = document.querySelector('#btn')
+    const jsonBtn = document.querySelector('#json')
+    const xmlBtn = document.querySelector('#xml')
+    const htmlBtn = document.querySelector('#html')
+    const result = document.querySelector('#result')
     let variants = null
 
     showInfo(variantsContainer, statContainer)
@@ -68,4 +105,30 @@ window.onload = () => {
             console.error('Ошибка:', error);
         }
     })
+
+    jsonBtn.addEventListener('click', async () => {
+        const data = await getJSON()
+        let variants = await fetch('/variants')
+        if (variants.ok) {
+            variants = await variants.json();
+        }
+        let str = ''
+        for (let key in data) {
+            str += variants[key] + ': ' + data[key] + '\n'
+        }
+
+        result.value = str
+    })
+
+    xmlBtn.addEventListener('click', async () => {
+        const data = await getXML()
+        result.value = data
+    })
+
+    htmlBtn.addEventListener('click', async () => {
+        const data = await getHTML()
+        result.value = data
+    })
 }
+
+
